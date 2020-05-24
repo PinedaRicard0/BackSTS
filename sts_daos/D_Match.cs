9282bf2c6f1597c6cecb475b5dbe17b8b1bfc5cf
Ricardo Pineda Rubio <pinedaricardo07@gmail.com>
2020-05-24 16:35:48 -0500

    Create D_Match.cs
    I forgot to upload this file in the last commit

---
+++ sts_daos/D_Match.cs
@@ -0,0 +1,25 @@
+﻿using sts_i_daos;
+using sts_models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sts_daos
+{
+    public class D_Match : ID_Match
+    {
+        private readonly DataContext _context;
+
+        public D_Match(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+
+        public async Task CreateMatch(Match match)
+        {
+            await _context.Matches.AddAsync(match);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
